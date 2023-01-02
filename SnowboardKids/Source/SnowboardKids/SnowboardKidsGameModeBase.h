72dac68f36a28a7cfca1caad9082134e9401372a
Lordmatics <niallmaple@yahoo.co.uk>
2023-01-02 17:05:04 +0000

    Initial Commit
    Setting up a blank unreal project.

---
+++ SnowboardKids/Source/SnowboardKids/SnowboardKidsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SnowboardKidsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNOWBOARDKIDS_API ASnowboardKidsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
